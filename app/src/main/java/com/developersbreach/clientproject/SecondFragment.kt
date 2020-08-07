40eab3644e744b4776552c6470fa7a6a340e39ad
Rajasekhar <rajashekarraju123@gmail.com>
2020-08-07 23:48:08 +0530

    Displays the available number of customers in queue

--- app/src/main/java/com/developersbreach/clientproject/SecondFragment.kt
+++ app/src/main/java/com/developersbreach/clientproject/SecondFragment.kt
@@ -1,31 +1,55 @@
 package com.developersbreach.clientproject
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
+import com.developersbreach.clientproject.databinding.FragmentSecondBinding
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class SecondFragment : Fragment() {
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+    private lateinit var binding: FragmentSecondBinding
+    private lateinit var firestore: FirebaseFirestore
+    private lateinit var adapter: CustomersAdapter
+    private lateinit var query: Query
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Access a Cloud Firestore instance from your Activity
+        firestore = FirebaseFirestore.getInstance()
+        query = firestore.collection("customers")
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSecondBinding.inflate(inflater)
+        return binding.root
+    }
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        adapter = CustomersAdapter(query)
+        binding.recyclerView.adapter = adapter
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Start listening for Firestore updates
+        adapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
     }
 }
