bdeddca0cbc5487f0ad19ddd2793e60cf256558d
Rajasekhar <rajashekarraju123@gmail.com>
2020-08-24 23:56:49 +0530

    Fragment to show app settings with settings compat

---
+++ app/src/main/java/com/developersbreach/clientproject/view/settings/SettingsFragment.kt
@@ -0,0 +1,40 @@
+package com.developersbreach.clientproject.view.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.developersbreach.clientproject.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+
+    private lateinit var binding: FragmentSettingsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.toolbarSettings.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val fragment = SettingsCompatFragment()
+        val fragmentManager = childFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.settingsFragmentCompatContainer.id, fragment)
+        fragmentTransaction.commit()
+    }
+}
