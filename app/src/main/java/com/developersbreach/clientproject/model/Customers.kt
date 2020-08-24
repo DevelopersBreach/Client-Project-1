6d4cfd953d5e4c148e8ad6c1972a02ebfd028c32
Rajasekhar <rajashekarraju123@gmail.com>
2020-08-25 00:06:37 +0530

    Model classes Customers and UserAccount to show user, customer data

---
+++ app/src/main/java/com/developersbreach/clientproject/model/Customers.kt
@@ -0,0 +1,78 @@
+package com.developersbreach.clientproject.model
+
+import android.os.Parcel
+import android.os.Parcelable
+import com.google.firebase.firestore.IgnoreExtraProperties
+
+
+@IgnoreExtraProperties
+class Customers : Parcelable {
+
+    var billNumber: String? = null
+    var totalItems: String? = null
+    var itemType: String? = null
+    var name: String? = null
+    var email: String? = null
+    var contact: String? = null
+    var status: Boolean = false
+    var date: String? = null
+
+    // Requires empty constructor for firestore
+    constructor()
+
+    internal constructor(
+        billNumber: String,
+        totalItems: String,
+        itemType: String,
+        customerName: String,
+        email: String,
+        contact: String,
+        status: Boolean,
+        date: String
+    ) : this() {
+        this.billNumber = billNumber
+        this.totalItems = totalItems
+        this.itemType = itemType
+        this.name = customerName
+        this.email = email
+        this.contact = contact
+        this.status = status
+        this.date = date
+    }
+
+    private constructor(`in`: Parcel) {
+        billNumber = `in`.readString()
+        totalItems = `in`.readString()
+        itemType = `in`.readString()
+        name = `in`.readString()
+        email = `in`.readString()
+        contact = `in`.readString()
+        status = `in`.readBoolean()
+        date = `in`.readString()
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(billNumber)
+        dest.writeString(totalItems)
+        dest.writeString(itemType)
+        dest.writeString(name)
+        dest.writeString(email)
+        dest.writeString(contact)
+        dest.writeBoolean(status)
+        dest.writeString(date)
+    }
+
+    companion object CREATOR : Parcelable.Creator<Customers> {
+        override fun createFromParcel(parcel: Parcel): Customers {
+            return Customers(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Customers?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
