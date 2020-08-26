0e0f124777a3841826e8c71cf1d6c03bc93f0dd7
Rajasekhar <rajashekarraju123@gmail.com>
2020-08-26 23:56:45 +0530

    New constants added to file

--- app/src/main/java/com/developersbreach/clientproject/utils/Constants.kt
+++ app/src/main/java/com/developersbreach/clientproject/utils/Constants.kt
@@ -1,5 +1,13 @@
 package com.developersbreach.clientproject.utils
 
+import com.firebase.ui.auth.AuthUI
+
 
 const val COLLECTION_PATH: String = "customers"
 const val FIELD_MAIL: String = "email"
+
+const val SIGN_IN_REQUEST_CODE = 9001
+val PROVIDERS = arrayListOf(
+    AuthUI.IdpConfig.GoogleBuilder().build(),
+    AuthUI.IdpConfig.EmailBuilder().build()
+)
