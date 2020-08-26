20c95f69f995dff0121bcfd57541ba627719c5d9
Rajasekhar <rajashekarraju123@gmail.com>
2020-08-27 04:31:29 +0530

    Added new app constants

--- app/src/main/java/com/developersbreach/clientproject/utils/Constants.kt
+++ app/src/main/java/com/developersbreach/clientproject/utils/Constants.kt
@@ -6,6 +6,9 @@ import com.firebase.ui.auth.AuthUI
 const val COLLECTION_PATH: String = "customers"
 const val FIELD_MAIL: String = "email"
 
+const val PREFERENCE_KEY_USER_ACCOUNT: String = "UserAccountPreferenceKey"
+const val PREFERENCE_KEY_USER_LOGOUT: String = "UserLogoutPreferenceKey"
+
 const val SIGN_IN_REQUEST_CODE = 9001
 val PROVIDERS = arrayListOf(
     AuthUI.IdpConfig.GoogleBuilder().build(),
