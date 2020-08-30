ff1e7a2604ab48863a15179f3f389fde344654f5
Rajasekhar <rajashekarraju123@gmail.com>
2020-08-30 23:48:20 +0530

    New constants for firestore fields of type array and string, added app resources

--- app/src/main/java/com/developersbreach/clientproject/utils/Constants.kt
+++ app/src/main/java/com/developersbreach/clientproject/utils/Constants.kt
@@ -4,10 +4,12 @@ import com.firebase.ui.auth.AuthUI
 
 
 const val COLLECTION_PATH: String = "customers"
+const val FIELD_MAIL: String = "email"
+
 const val COLLECTION_PATH_PHOTOS: String = "shop"
 const val DOCUMENT_IMAGES: String = "images"
-const val FIELD_MAIL: String = "email"
-const val FIELD_IMAGES: String = "shopImages"
+const val FIELD_ARRAY_IMAGES: String = "images_array"
+
 
 const val PREFERENCE_KEY_USER_ACCOUNT: String = "UserAccountPreferenceKey"
 const val PREFERENCE_KEY_USER_LOGOUT: String = "UserLogoutPreferenceKey"
