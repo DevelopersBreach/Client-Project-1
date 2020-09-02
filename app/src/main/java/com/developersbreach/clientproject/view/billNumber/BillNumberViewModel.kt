3dd7f16fe3b6ca7ca497f7259fc48b182d5db526
Rajasekhar <rajashekarraju123@gmail.com>
2020-09-02 23:59:15 +0530

    Removed variable leaking cause of context, object renaming, code clean-up

--- app/src/main/java/com/developersbreach/clientproject/view/billNumber/BillNumberViewModel.kt
+++ app/src/main/java/com/developersbreach/clientproject/view/billNumber/BillNumberViewModel.kt
@@ -1,21 +1,19 @@
 package com.developersbreach.clientproject.view.billNumber
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
 import com.developersbreach.clientproject.R
 import com.google.android.material.textfield.TextInputLayout
 
-class BillNumberViewModel(application: Application) : AndroidViewModel(application) {
-
-    private val context = application.applicationContext
+class BillNumberViewModel: ViewModel() {
 
     fun validateBillNumber(
         text: String?,
         textInputLayout: TextInputLayout
     ): String {
+        textInputLayout.context
         if (text?.isEmpty()!!) {
             textInputLayout.isErrorEnabled = true
-            textInputLayout.error = context.getString(R.string.field_required_error_text)
+            textInputLayout.error = textInputLayout.context.getString(R.string.field_required_error_text)
         } else if (text.isNotEmpty()) {
             textInputLayout.isErrorEnabled = false
         }
