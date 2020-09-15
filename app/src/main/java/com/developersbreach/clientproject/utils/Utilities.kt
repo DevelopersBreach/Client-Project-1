7a8b163fb45add64a6537b5798de3c5127906545
Rajasekhar <rajashekarraju123@gmail.com>
2020-09-15 23:58:48 +0530

    Included functions to hide and show status bar in MainActivity.kt with destination change listener

--- app/src/main/java/com/developersbreach/clientproject/utils/Utilities.kt
+++ app/src/main/java/com/developersbreach/clientproject/utils/Utilities.kt
@@ -1,9 +1,21 @@
 package com.developersbreach.clientproject.utils
 
+import android.app.Activity
 import android.util.TypedValue
 import android.view.View
 
 
 fun View.convertToDp(value: Float): Int {
-    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, resources.displayMetrics).toInt()
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, resources.displayMetrics)
+        .toInt()
+}
+
+fun showStatusBar(activity: Activity) {
+    @Suppress("DEPRECATION")
+    activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+}
+
+fun hideStatusBar(activity: Activity) {
+    @Suppress("DEPRECATION")
+    activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 }
--- app/src/main/java/com/developersbreach/clientproject/view/controller/MainActivity.kt
+++ app/src/main/java/com/developersbreach/clientproject/view/controller/MainActivity.kt
@@ -6,12 +6,24 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.developersbreach.clientproject.R
 import com.developersbreach.clientproject.databinding.ActivityMainBinding
+import com.developersbreach.clientproject.utils.hideStatusBar
+import com.developersbreach.clientproject.utils.showStatusBar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        this.findNavController(R.id.nav_host_fragment)
+
+        val activity = this
+        activity.findNavController(R.id.nav_host_fragment).apply {
+            this.addOnDestinationChangedListener { _, destination, _ ->
+                if (destination.id == R.id.introFragment) {
+                    hideStatusBar(activity)
+                } else {
+                    showStatusBar(activity)
+                }
+            }
+        }
     }
 }
