c88f327b90815fa4e4b0db38ce24c022d3f6a9ff
Rajasekhar <rajashekarraju123@gmail.com>
2020-11-12 23:49:44 +0530

    Created application class for app, planted timber, added permissions to manifest

---
+++ app/src/main/java/com/developersbreach/clientproject/ShivaApplication.kt
@@ -0,0 +1,12 @@
+package com.developersbreach.clientproject
+
+import android.app.Application
+import timber.log.Timber
+
+class ShivaApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
